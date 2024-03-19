51898a8437fb4f52a3f497bfb895c1ce24c7ebd5
Yusuf <95586079+Byqb@users.noreply.github.com>
2024-03-19 11:40:07 +0300

    go

--- rev.go
+++ rev.go
@@ -1,6 +1,44 @@
-<----- alphamirror ----->
-package checkpoint
+<----- atoi ----->
+package main
+func Atoi(s string) (int, bool) {
+	sign := 1
+	res := 0
+	err := false
 
+	if s[0] == '-' {
+		sign = -1
+		s = s[1:]
+	}
+	for _, e := range s {
+		if e >= '0' && e <= '9' {
+			res = res*10 + int(e-48)
+		} else {
+			err = true
+		}
+	}
+	return res * sign, err
+}
+
+<----- itoa ----->
+package main
+func Itoa(i int) string {
+	if i == 0 {
+		return "0"
+	}
+	res := ""
+	for i > 0 {
+		res = string((i%10 + 48)) + res
+		i /= 10
+	}
+	if i < 0 {
+		return "-" + Itoa(-i)
+	} else {
+		return res
+	}
+}
+
+<----- alphamirror ----->
+package main
 func Alphamirror(s string) string {
 	res := ""
 	for _, e := range s {
@@ -16,13 +54,7 @@ func Alphamirror(s string) string {
 }
 ----------or------------
 package main
-
-import (
-	"os"
-
-	"github.com/01-edu/z01"
-)
-
+import ("os""github.com/01-edu/z01")
 func main() {
 	if len(os.Args[1:]) != 1 {
 		z01.PrintRune(10)
@@ -116,6 +148,87 @@ func main() {
 }
 
 
+<----- chunk ----->
+package main
+import "fmt"
+func Chunk(a []int, ch int) {
+	if ch <= 0 {
+		return
+	}
+	var slice []int
+	result := make([][]int, 0, len(a)/ch+1)
+	for len(a) >= ch {
+		slice, a = a[:ch], a[ch:]
+		result = append(result, slice)
+	}
+	if len(a) > 0 {
+		result = append(result, a)
+	}
+	fmt.Println(result)
+}
+
+<-----displayfirstparam----->
+
+package main
+import (
+	"os"
+	"github.com/01-edu/z01"
+)
+func main() {
+	if len(os.Args) > 1 {
+		firstArg := os.Args[1]
+		for _, value := range firstArg {
+			z01.PrintRune(value)
+		}
+		z01.PrintRune('\n')
+	}
+}
+<----- displaylastparam ----->
+
+package main
+import (
+	"os"
+	"github.com/01-edu/z01"
+)
+func main() {
+	printstr(os.Args[len(os.Args)-1])
+	z01.PrintRune('\n')
+	for _, e := range s {
+		z01.PrintRune(e)
+	}
+}
+<----- printbits ----->
+package main
+
+import "github.com/01-edu/z01"
+func PrintBits(octet byte) {
+	for i := 7; i >= 0; i-- {
+		if octet&(1<<uint(i)) != 0 {
+			z01.PrintRune('1')
+		} else {
+			z01.PrintRune('0')
+		}
+	}
+}
+<----- paramcount ----->
+
+package main
+import ("os" "github.com/01-edu/z01")
+func main() {
+	// z01.PrintRune()                                 This prints a rune
+	// len(os.Args) - 1 + '0'                          This takes the length of arguments
+	// rune(len(os.Args) - 1 + '0')                    This converts the length of arguments to a rune
+	// z01.PrintRune(rune(len(os.Args) - 1 + '0'))     Everything together
+	z01.PrintRune(rune(len(os.Args) - 1 + '0'))
+	z01.PrintRune('\n')
+}
+<----- powerof2 ----->
+package main
+func isPowerOf2(n int) bool {
+	return n != 0 && ((n & (n - 1)) == 0)
+}
+
+
 <----- wdmatch ----->
 package main
 
@@ -151,7 +264,6 @@ func main() {
 			z01.PrintRune('\n')
 		}
 	}
-
 }
 
 <----- swapbits ----->
@@ -187,7 +299,6 @@ func Tabmult(nbr int) {
 		z01.PrintRune('\n')
 	}
 }
-
 func printstr(s string) {
 	for _, r := range s {
 		z01.PrintRune(r)
@@ -209,8 +320,10 @@ func itoa(i int) string {
 		return "-" + itoa(-i)
 	}
 	return result
+
 }
 <----- paramcount ----->
+
 package main
 import (
 	"os"
@@ -220,21 +333,16 @@ func main() {
 	z01.PrintRune(rune(len(os.Args) - 1 + '0'))
 	z01.PrintRune('\n')
 }
+
 <----- lastword ----->
+
 package main
-
-import (
-	"checkpoint"
-	"os"
-
-	"github.com/01-edu/z01"
-)
-
+import ("os""github.com/01-edu/z01")
 func main() {
 	if len(os.Args) != 2 {
 		return
 	}
-	checkpoint.PrintStr(lastWord(os.Args[1]))
+	PrintStr(lastWord(os.Args[1]))
 	z01.PrintRune('\n')
 }
 func rmEndSpace(s string) string {
@@ -255,29 +363,25 @@ func lastWord(s string) string {
 	}
 	return result
 }
+
 <----- displayFirstParam ----->
+
 package main
-
-import (
-	"os"
-
-	"github.com/01-edu/z01"
-)
+import ("os""github.com/01-edu/z01")
 func main() {
 	if len(os.Args) > 1 {
-		printstr(os.Args[1])
+		firstArg := os.Args[1]
+		for _, value := range firstArg {
+			z01.PrintRune(value)
+		}
+		z01.PrintRune('\n')
 	}
 }
-func printstr(s string) {
-	for _, e := range s {
-		z01.PrintRune(e)
-	}
-}
+
 <-----fishandchips----->
+
 package main
-
 import "fmt"
-
 func FishAndChips(n int) string {
 	if n%2 == 0 && n%3 == 0 {
 		return "fish and chips"
@@ -296,10 +400,52 @@ func FishAndChips(n int) string {
 	}
 	return ""
 }
-<-----  ----->
 
-<-----  ----->
+<----- recursivesum ----->
 
+package main
+func RecursiveSum(n int) int {
+	if n == 0 {
+		return 0
+	}
+	return n + RecursiveSum(n-1)
+}
+
+<----- reduceint ----->
+package main
+import "strconv"
+// func ReduceInt(a []int, f func(int, int) int) {
+// 	firstNum := a[0]
+// 	secondNum := a[1]
+// 	result := f(firstNum, secondNum)
+// 	PrintStr(strconv.Itoa(result))
+// }
+// shorter version
+func ReduceInt(a []int, f func(int, int) int) {
+	PrintStr(strconv.Itoa(f(a[0], a[1])))
+
+}
+<----- rot13 ----->
+package main
+func Rot13(s string) string {
+	res := ""
+	for _, e := range s {
+		if e >= 'A' && e <= 'Z' {
+			res += string((e-'A'+13)%26 + 'A')
+		} else if e >= 'a' && e <= 'z' {
+			res += string((e-'a'+13)%26 + 'a')
+		} else {
+			res += string(e)
+		}
+	}
+	return res
+}
+
+<-----  swapbits ----->
+package main
+func SwapBits(octet byte) byte {
+	return octet<<4 | octet>>4
+}
 <-----  ----->
 
 <-----  ----->
